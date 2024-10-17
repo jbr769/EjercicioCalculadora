0359337fb77c5802172841d11a540945bd76a2ed
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-10-17 19:00:54 +0200

    Initial commit

---
+++ app/src/main/java/com/learnwithjahangirjadi/calculadora/ui/theme/Theme.kt
@@ -0,0 +1,41 @@
+package com.learnwithjahangirjadi.calculadora.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color.Black,
+    secondary = orange,
+    tertiary = blueGray,
+    error = gray,
+    background = Color.Black
+)
+
+@Composable
+fun Calculadora(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
